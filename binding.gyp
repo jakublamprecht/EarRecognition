846e8d3e93a75657bbb6175f92eede34d8a5c73e
Jakub Lamprecht <jakub.lamprecht@gmail.com>
2018-07-14 15:11:33 +0200

    Added OpenCV dependency to addon build

--- binding.gyp
+++ binding.gyp
@@ -1,8 +1,14 @@
 {
   "targets": [{
     "target_name": "earImgProcessing",
-    "include_dirs" : [
-      "<!(node -e \"require('nan')\")"
+    "include_dirs": [
+      "<!(node -e \"require('nan')\")",
+      "<(module_root_dir)/vendor/OpenCV/include",
+      "<(module_root_dir)/vendor/OpenCV/include/opencv",
+      "<(module_root_dir)/vendor/OpenCV/include/opencv2"
+    ],
+		"libraries": [
+      "<(module_root_dir)/vendor/OpenCV/x64/vc14/lib/opencv_world320.lib"
     ],
     "sources": [
       "./src/cpp/main.cc"
--- src/cpp/main.cc
+++ src/cpp/main.cc
@@ -1,10 +1,24 @@
 #include <nan.h>
+#include <opencv2/opencv.hpp>
+
+void showImg() {
+  cv::Mat myMat;
+
+  myMat = cv::imread("../../assets/img/450x650.png", 0);
+
+  cv::namedWindow("My test window", 0);
+  cv::imshow("My test window", myMat);
+
+  cv::waitKey(0);
+}
 
 void Method(const Nan::FunctionCallbackInfo<v8::Value>& info) {
   info.GetReturnValue().Set(Nan::New("world").ToLocalChecked());
 }
 
 void Init(v8::Local<v8::Object> exports) {
+  showImg();
+
   exports->Set(Nan::New("hello").ToLocalChecked(),
                Nan::New<v8::FunctionTemplate>(Method)->GetFunction());
 }
